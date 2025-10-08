363f4664275a8b1067ab512391e024c6872a320e
Jhonzn <jhonfernanados@gmail.com>
2025-10-08 14:56:16 -0500

    Configure model

--- server-book/models/Usuario.js
+++ server-book/models/Usuario.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     
-    nombre: { type: String, require: true, unique: true},
-    email: { type: String, require: true, unique: true},
-    password: { type: String, require: true}
+    nombre: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true}
 
 });
 
